bea5c530e6407974ddcc884127f54198580686fa
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-14 13:11:43 +0300

    Create unmute.js

---
+++ komutlar/unmute.js
@@ -0,0 +1,15 @@
+module.exports = {
+name:"unmute",
+code:`
+$sendDM[$mentioned[1];$customEmoji[unmute] **$serverName** sunucusunda mute süren bitmiştir. Tekrardan kanallara yazabilirsin.]
+$channelSendMessage[$channelID;{author:Üye mutesi kaldırıldı!:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üyenin mutesi kaldırıldı ve <@&$replaceText[$replaceText[$replaceText[$getServerVar[cinsiyet;$authorID];erkek;$getServerVar[erkek];-1];kız;$getServerVar[kız];-1];kayıtsız;$getServerVar[kayıtsız];-1]> rolü verildi}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+
+$setUserVar[mute;no;$mentioned[1]]
+$giveRoles[$mentioned[1];$replaceText[$replaceText[$replaceText[$getServerVar[cinsiyet;$authorID];erkek;$getServerVar[erkek];-1];kız;$getServerVar[kız];-1];kayıtsız;$getServerVar[kayıtsız];-1]]
+$takeRoles[$mentioned[1];$getServerVar[muterol]]
+
+$onlyIf[$hasRole[$mentioned[1];$getServerVar[muterol]]==true;$getServerVar[cross] <@$authorID>, Üye zaten mutelenmemiş.]
+$onlyIf[$mentioned[1]!=;$getServerVar[cross] <@$authorID>, Kimin mutesini kaldırmalıyım?]
+$onlyForRoles[$getServerVar[mutehammer];$getServerVar[cross] <@$authorID>, Bunu kullanamazsın.]
+`
+} 
