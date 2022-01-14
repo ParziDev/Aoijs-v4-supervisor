cd6ba6416b7bb148f7b117ef094013231a6d1469
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-14 12:49:06 +0300

    Create avatar.js

---
+++ komutlar/avatar.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name:"avatar",
+  aliases:["av","pp"],
+  code:`
+  $description[[Avatar linki]($userAvatar[$mentioned[1;yes]])]
+  $image[$userAvatar[$mentioned[1;yes]]]
+  $color[BLACK]
+  $footer[$userTag tarafından istendi]
+  `
+}
