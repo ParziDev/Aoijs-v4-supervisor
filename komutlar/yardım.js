ae311a7227137ce7da433613cb436143ed9fc0e6
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-14 13:13:17 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name:"yardım",
+  code:`
+  $author[Selam $userTag;$authorAvatar]
+  $addField[Özel komutlar;\`eval\` | \`vip ver\` | \`vip al\`]
+  $addField[Kayıt komutları;\`erkek\` | \`kız\` | \`isim\` | \`kayıtsız\` | \`kayıt-sayı\` | \`top\`]
+  $addField[Moderasyon komutları;\`ban\` | \`ban-sorgula\` | \`unban\` | \`jail\` | \`unjail\` | \`mute\` | \`unmute\`]
+  $addField[Genel komutlar;\`yardım\` | \`tag\` | \`say\` | \`avatar\`]
+  $color[BLACK]
+  $thumbnail[$authorAvatar]
+  `
+}
