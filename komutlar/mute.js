ba279324887bb96f52613a76c01cab22aae9a21e
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-14 13:04:42 +0300

    Create mute.js

---
+++ komutlar/mute.js
@@ -0,0 +1,31 @@
+module.exports = {
+name:"mute",
+aliases:"sustur",
+code:`
+$sendDM[$mentioned[1];$customEmoji[unmute] **$serverName** sunucusunda mute süren bitmiştir. Tekrardan kanallara yazabilirsin.]
+$onlyIf[$getUserVar[mute;$mentioned[1]]!=no;]
+$channelSendMessage[$channelID;$customEmoji[unmute] <@$mentioned[1]>, Mute süren bitti. Tekrardan kanallara yazabilirsin.]
+$setUserVar[mute;no;$mentioned[1]]
+$giveRoles[$mentioned[1];$replaceText[$replaceText[$replaceText[$getServerVar[cinsiyet;$authorID];erkek;$getServerVar[erkek];-1];kız;$getServerVar[kız];-1];kayıtsız;$getServerVar[kayıtsız];-1]]
+$takeRoles[$mentioned[1];$getServerVar[muterol]]
+
+$wait[$noMentionMessage[1]]
+
+$sendDM[$mentioned[1];$customEmoji[mute] **$serverName** sunucusunda "$replaceText[$noMentionMessage;$noMentionMessage[1];;-1] " sebebinden mutelendin.]
+$channelSendMessage[$channelID;{author:Üye mutelendi!:$userAvatar[$mentioned[1]]}{description:$customEmoji[mute] <@$mentioned[1]> adlı üye **$noMentionMessage[1]** boyunca "$replaceText[$noMentionMessage;$noMentionMessage[1];;-1] " sebebinden susturuldu}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+
+$setUserVar[mute;yes;$mentioned[1]]
+$giveRoles[$mentioned[1];$getServerVar[muterol]]
+$takeRoles[$mentioned[1];$getServerVar[erkek];$getServerVar[kız];$getServerVar[kayıtsız]]
+
+$onlyIf[$noMentionMessage[2]!=;$getServerVar[cross] <@$authorID>, Mute sebebini girin.]
+$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$noMentionMessage[1];s;];m;];h;];d;]]!=false;$getServerVar[cross] <@$authorID>, Mute süresini giriniz.]
+$onlyIf[$noMentionMessage[1]!=;$getServerVar[cross] <@$authorID>, Mute süresini girin.]
+$onlyIf[$hasRole[$mentioned[1];$getServerVar[muterol]]!=true;$getServerVar[cross] <@$authorID>, Üye zaten mutelenmiş.]
+$onlyIf[$rolePosition[$highestRole[$authorID]]<=$rolePosition[$highestRole[$mentioned[1]]];$getServerVar[cross] <@$authorID>, rolünden üstte veya eşit birini muteleyemezsin.]
+$onlyIf[$mentioned[1]!=$authorID;$getServerVar[cross] <@$authorID>, Kendini muteleyemezsin.]
+$onlyIf[$mentioned[1]!=;$getServerVar[cross] <@$authorID>, Kimi mutelemeliyim?]
+$onlyForRoles[$getServerVar[mutehammer];$getServerVar[cross] <@$authorID>, Bunu kullanamazsın.]
+$suppressErrors
+`
+}
