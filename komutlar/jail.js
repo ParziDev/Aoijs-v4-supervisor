5bcb25a0caaa5537b05c92cc73e26489abe54269
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-14 13:00:26 +0300

    Update jail.js

--- komutlar/jail.js
+++ komutlar/jail.js
@@ -2,8 +2,8 @@ module.exports = {
   name:"jail",
   aliases:"hapis",
   code:`
-$sendDM[$mentioned[1];$customEmoji[jail] **$serverName** sunucusunda "$noMentionMessage[1]" sebebinden jaile atıldın.]
-$channelSendMessage[$channelID;{author:Üye jaile atıldı!:$userAvatar[$mentioned[1]]}{description:$customEmoji[jail] <@$mentioned[1]> adlı üye "$noMentionMessage" sebebinden jaile atıldı}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+$sendDM[$mentioned[1];**$serverName** sunucusunda "$noMentionMessage[1]" sebebinden jaile atıldın.]
+$channelSendMessage[$channelID;{author:Üye jaile atıldı!:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üye "$noMentionMessage" sebebinden jaile atıldı}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
 $setRoles[$mentioned[1];$getServerVar[jailrol]]
 $setUserVar[jail;yes;$mentioned[1]]
 $onlyIf[$noMentionMessage[1]!=;$getServerVar[cross] <@$authorID>, Jail sebebini gir.]
