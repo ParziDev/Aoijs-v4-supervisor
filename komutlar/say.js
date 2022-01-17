12210decc60e0ec627b2d587b43a0f6a996559ba
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-17 16:09:37 +0300

    Update say.js

--- komutlar/say.js
+++ komutlar/say.js
@@ -1,20 +1,10 @@
 module.exports = {
-  name:"say",
-  code:`
-***Sunucumuzda \`$membersCount\` üye bulunmakta.
-Sunucumuzda \`$math[$membersCount-$membersCount[$guildID;offline]]\` çevrimiçi üye bulunmakta.
-Sunucumuzda \`$roleMembersCount[$getServerVar[tagrol]]\` taglı bulunmakta.
-Sunucumuzda \`$serverBoostCount\` takviye bulunmakta.***
-$djsEval[
-  function sesSay() {
-const voiceChannels = message.guild.channels.cache.filter(c => c.type === 'voice');
-let count = 0;
-
-for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
-
-return count;
-}
-message.channel.send("***Sesli kanallarda toplam \`" + sesSay() + "\` üye bulunuyor.***")] 
-
-  `
-}
+name:"say",
+code:`
+$description[\`•\` Sesli kanallarda toplam **$voiceMembersCount** üye bulunmakta
+\`•\` Sunucumuzda toplam **$membersCount** üye bulunmakta
+\`•\` Sunucumuzda **$roleMembersCount[$getServerVar[tagrol]]** taglı üye bulunmakta
+\`•\` Sunucumuza toplam **$serverBoostCount** boost basılmış]
+$color[BLACK]
+`
+} 
