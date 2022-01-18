0de0a207499fdc7abb56ab31ff9646d32eaba86a
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-18 14:48:41 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -5,7 +5,7 @@ module.exports = {
   $addField[Özel komutlar;\`eval\` | \`vip ver\` | \`vip al\`]
   $addField[Kayıt komutları;\`erkek\` | \`kız\` | \`isim\` | \`kayıtsız\` | \`kayıt-sayı\` | \`top\`]
   $addField[Moderasyon komutları;\`ban\` | \`ban-sorgula\` | \`unban\` | \`jail\` | \`unjail\` | \`mute\` | \`unmute\`]
-  $addField[Genel komutlar;\`yardım\` | \`tag\` | \`say\` | \`avatar\`]
+  $addField[Genel komutlar;\`yardım\` | \`tag\` | \`say\` | \`avatar\` | \`nerede\`]
   $color[BLACK]
   $thumbnail[$authorAvatar]
   `
