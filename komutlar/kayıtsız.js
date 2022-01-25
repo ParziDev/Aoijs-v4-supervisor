b059b71fd2164f2dcd1a1b10b121f3769543ecc3
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-26 00:03:28 +0300

    Update kayıtsız.js

--- komutlar/kayıtsız.js
+++ komutlar/kayıtsız.js
@@ -1,9 +1,10 @@
 module.exports = {
   name:"kayıtsız",
   code:`
-  $channelSendMessage[$channelID;{author:Üye kayıtsıza atıldı:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üyeye <@&$getServerVar[kayıtsız]> rolü verildi ve tüm rolleri alındı}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+  $channelSendMessage[$channelID;{author:Üye kayıtsıza atıldı:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üyeye <@&$getServerVar[kayıtsız]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
   $changeNickname[$mentioned[1];$getServerVar[tag] İsim | Yaş]
-  $setRoles[$mentioned[1];$getServerVar[kayıtsız]]
+  $giveRoles[$mentioned[1];$getServerVar[kayıtsız]]
+  $takeRoles[$mentioned[1];$getServerVar[erkek];$getServerVar[kız]]
   $onlyIf[$rolePosition[$highestRole[$authorID]]<=$rolePosition[$highestRole[$mentioned[1]]];$getServerVar[cross] <@$authorID>, rolünden üstte veya eşit birini kayıtsıza atamazsın.]
   $onlyIf[$mentioned[1]!=$authorID;$getServerVar[cross] <@$authorID>, Kendini kayıtsıza atamazsın.]
   $onlyIf[$mentioned[1]!=;$getServerVar[cross] <@$authorID>, Kimi kayıtsıza atmalıyım?]
