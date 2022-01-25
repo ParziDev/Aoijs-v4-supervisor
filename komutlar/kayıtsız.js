534b16f8cc0633d15c43f79a7a68b351bb0d9bc1
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-26 00:07:31 +0300

    Update kayıtsız.js

--- komutlar/kayıtsız.js
+++ komutlar/kayıtsız.js
@@ -2,7 +2,8 @@ module.exports = {
   name:"kayıtsız",
   code:`
   $channelSendMessage[$channelID;{author:Üye kayıtsıza atıldı:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üyeye <@&$getServerVar[kayıtsız]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
-  $changeNickname[$mentioned[1];$getServerVar[tag] İsim | Yaş]
+  $changeNickname[$mentioned[1];• İsim | Yaş]
+  $setUserVar[cinsiyet;kayıtsız;$mentioned[1]]
   $giveRoles[$mentioned[1];$getServerVar[kayıtsız]]
   $takeRoles[$mentioned[1];$getServerVar[erkek];$getServerVar[kız]]
   $onlyIf[$rolePosition[$highestRole[$authorID]]<=$rolePosition[$highestRole[$mentioned[1]]];$getServerVar[cross] <@$authorID>, rolünden üstte veya eşit birini kayıtsıza atamazsın.]
