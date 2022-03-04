8afe4e0efac2697bb1ea79937aba93f7b4d9391e
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-03-04 18:03:58 +0300

    Update unmute.js

--- komutlar/unmute.js
+++ komutlar/unmute.js
@@ -2,7 +2,7 @@ module.exports = {
 name:"unmute",
 code:`
 $sendDM[$mentioned[1];**$serverName** sunucusunda mute süren bitmiştir. Tekrardan kanallara yazabilirsin.]
-$channelSendMessage[$channelID;{author:Üye mutesi kaldırıldı!:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üyenin mutesi kaldırıldı ve <@&$replaceText[$replaceText[$replaceText[$getServerVar[cinsiyet;$authorID];erkek;$getServerVar[erkek];-1];kız;$getServerVar[kız];-1];kayıtsız;$getServerVar[kayıtsız];-1]> rolü verildi}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+$channelSendMessage[$channelID;{author:Üye mutesi kaldırıldı!:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üyenin mutesi kaldırıldı ve <@&$replaceText[$replaceText[$replaceText[$getUserVar[cinsiyet;$mentioned[1]];erkek;$getServerVar[erkek];-1];kız;$getServerVar[kız];-1];kayıtsız;$getServerVar[kayıtsız];-1]> rolü verildi}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
 
 $setUserVar[mute;no;$mentioned[1]]
 $giveRoles[$mentioned[1];$replaceText[$replaceText[$replaceText[$getUserVar[cinsiyet;$mentioned[1]];erkek;$getServerVar[erkek];-1];kız;$getServerVar[kız];-1];kayıtsız;$getServerVar[kayıtsız];-1]]
