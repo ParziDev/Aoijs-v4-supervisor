90db0de14816c3a6466d5d7f90b02b3c7d3116f7
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-03-04 18:03:25 +0300

    Update unjail.js

--- komutlar/unjail.js
+++ komutlar/unjail.js
@@ -4,19 +4,19 @@ module.exports = {
   $if[$getUserVar[cinsiyet;$mentioned[1]]==erkek]
   $setRoles[$mentioned[1];$getServerVar[erkek]]
   $setUserVar[jail;no;$mentioned[1]]
-  $channelSendMessage[$channelID;{author:Üye jailden çıkarıldı!:$userAvatar[$mentioned[1]]}{description:$customEmoji[unjail] <@$mentioned[1]> adlı üye jailden çıkarıldı ve <@&$getServerVar[erkek]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+  $channelSendMessage[$channelID;{author:Üye jailden çıkarıldı!:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üye jailden çıkarıldı ve <@&$getServerVar[erkek]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
   $endif
 
   $if[$getUserVar[cinsiyet;$mentioned[1]]==kız]
   $setRoles[$mentioned[1];$getServerVar[kız]]
   $setUserVar[jail;no;$mentioned[1]]
-  $channelSendMessage[$channelID;{author:Üye jailden çıkarıldı!:$userAvatar[$mentioned[1]]}{description:$customEmoji[unjail] <@$mentioned[1]> adlı üye jailden çıkarıldı ve <@&$getServerVar[kız]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+  $channelSendMessage[$channelID;{author:Üye jailden çıkarıldı!:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üye jailden çıkarıldı ve <@&$getServerVar[kız]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
   $endif
   
   $if[$getUserVar[cinsiyet;$mentioned[1]]==kayıtsız]
   $setRoles[$mentioned[1];$getServerVar[kayıtsız]]
   $setUserVar[jail;no;$mentioned[1]]
-  $channelSendMessage[$channelID;{author:Üye jailden çıkarıldı!:$userAvatar[$mentioned[1]]}{description:$customEmoji[unjail] <@$mentioned[1]> adlı üye jailden çıkarıldı ve <@&$getServerVar[kayıtsız]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
+  $channelSendMessage[$channelID;{author:Üye jailden çıkarıldı!:$userAvatar[$mentioned[1]]}{description:<@$mentioned[1]> adlı üye jailden çıkarıldı ve <@&$getServerVar[kayıtsız]> rolü verildi.}{color:BLACK}{thumbnail:$userAvatar[$mentioned[1]]}]
   $endif
   
   $onlyIf[$hasRole[$mentioned[1];$getServerVar[jailrol]]==true;$getServerVar[cross] <@$authorID>, üye zaten jailde değil.]
